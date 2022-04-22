91ca48e59666095200fd140a1628437ff1b8a502
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-04-22 18:31:42 -0300

    identação

--- autentificacao/views.py
+++ autentificacao/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.contrib import auth, messages
 from django.contrib.auth.models import User
-from django.contrib import messages
 from django.contrib.messages import constants
-from django.contrib import auth
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
 
 def cadastro(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
             return redirect('/jobs/encontrar_jobs')
-        
+
         return render(request, 'cadastro.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
@@ -17,35 +17,41 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirm-password')
 
         if not senha == confirmar_senha:
-            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
+            messages.add_message(request, constants.ERROR,
+                                 'As senhas não coincidem')
             return redirect('/auth/cadastro')
-        
+
         if len(username.strip()) == 0 or len(senha.strip()) == 0:
-            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            messages.add_message(request, constants.ERROR,
+                                 'Preencha todos os campos')
             return redirect('/auth/cadastro')
-        
-        user = User.objects.filter(username = username)
+
+        user = User.objects.filter(username=username)
 
         if user.exists():
-            messages.add_message(request, constants.ERROR, 'Já existe um usário com esse username')
+            messages.add_message(request, constants.ERROR,
+                                 'Já existe um usário com esse username')
             return redirect('/auth/cadastro')
-        
+
         try:
 
-            user = User.objects.create_user(username = username, password = senha)
+            user = User.objects.create_user(username=username, password=senha)
             user.save()
-            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso')
+            messages.add_message(request, constants.SUCCESS,
+                                 'Usuário criado com sucesso')
 
             return redirect('/auth/logar')
 
         except:
-            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            messages.add_message(request, constants.ERROR,
+                                 'Erro interno do sistema')
             return redirect('/auth/cadastro')
 
         return HttpResponse('Recebido')
 
+
 def logar(request):
-    if  request.method == 'GET':
+    if request.method == 'GET':
         if request.user.is_authenticated:
             return redirect('/jobs/encontrar_jobs')
 
@@ -55,13 +61,15 @@ def logar(request):
         senha = request.POST.get('password')
 
         user = auth.authenticate(username=username, password=senha)
-        
+
         if not user:
-            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            messages.add_message(request, constants.ERROR,
+                                 'Username ou senha inválidos')
             return redirect('/auth/logar')
         else:
             auth.login(request, user)
             return redirect('/jobs/encontrar_jobs')
+
 
 def sair(request):
     auth.logout(request)
